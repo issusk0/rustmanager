9bfdfe496ffd298a1646743b7f789fc7155c1fec
ubuntu-miku <plazasjolifenzio@gmail.com>
2025-11-22 19:51:06 -0300

    actualizacion continua de procesos

--- rustmanager/src/utils.rs
+++ rustmanager/src/utils.rs
@@ -1,36 +1,47 @@
 use std::*;
-use sysinfo::{Pid, System};
+use std::time::Duration;
+use sysinfo::{Pid, Process, System};
 
 
-pub fn info(){
+pub fn info() {
     let mut sys = System::new_all();
-    sys.refresh_all();
-    let cpu_usage = sys.global_cpu_usage();
-    let core_cpus = sys.cpus().len();
-    let total_threads = std::thread::available_parallelism().unwrap();
-    let total_memory = sys.total_memory();
-    let free_memory = sys.free_memory();
-    println!("Total RAM Memory: {} MB", total_memory / 1024 / 1024);
-    println!("Free RAM memory: {} MB", free_memory / 1024 / 1024);
-    println!("Total CPU usage: {}% ", cpu_usage);
-    println!("Total core cpus: {}",core_cpus);
-    println!("Total Threads: {}",total_threads);
+    loop {
 
-    for (pid, process) in sys.processes() {
-        let name =process.name().to_string_lossy();
-        let status =process.status();
-        let memory = (process.memory()as f64) / 1024.0/1024.0 ;
-        let cpu_per_process = process.cpu_usage();
-        println!("\
-        PID: {}, \
-        Name: {}, \
-        Status: {}, \
-        Memory consumed:{:.0} MB,\
-        CPU USAGE: {} %", pid, name,status,memory, cpu_per_process);
+
+
+        sys.refresh_all();
+        let cpu_usage = sys.global_cpu_usage();
+        let core_cpus = sys.cpus().len();
+        let total_threads = std::thread::available_parallelism().unwrap();
+        let total_memory = sys.total_memory();
+        let free_memory = sys.available_memory();
+        println!("Total RAM Memory: {} MB", total_memory / 1024 / 1024);
+        println!("Available RAM memory: {} MB", free_memory / 1024 / 1024);
+        println!("Total CPU usage: {}% ", cpu_usage);
+        println!("Total core cpus: {}", core_cpus);
+        println!("Total Threads: {}", total_threads);
+
+        let mut processes_list: Vec<(&Pid, &Process)> = sys.processes().into_iter().collect();
+        processes_list.sort_by(|a, b| b.1.cpu_usage().partial_cmp(&a.1.cpu_usage()).unwrap());
+        for (pid, process) in processes_list.iter() {
+            let ram_usage = process.memory() / 1024 / 1024;
+            let proccess_name = process.name().to_string_lossy();
+            let cpu_usage = process.cpu_usage();
+            println!("\
+            PID: {}\
+            -Process Name: {}-\
+            CPU usage: {:.2}% \
+            RAM usage-{} MB\
+            \
+            ", pid,proccess_name, cpu_usage, ram_usage);
+        };
+        println!();
+        println!();
+        println!("Scanning processes....");
+        thread::sleep(Duration::from_millis(5000));
     }
+
 }
-
-
 pub fn killprocess(){
     let mut sys = System::new_all();
     let mut input_text = String::new();
